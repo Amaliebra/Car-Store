86783b2619b486d0eda3d3180de773477e26f7a6
Amaliebra <amaliebratland@gmail.com>
2025-01-09 12:16:17 +0100

    removed hardcoding, added switch case for user input

--- CarShopConsole/Program.cs
+++ CarShopConsole/Program.cs
@@ -6,21 +6,72 @@ namespace CarShopConsole
     {
         static void Main(string[] args)
         {
-            Car c = new Car("Volvo", "V70", 35000.0m);
-            Car d = new Car("Toyota", "Corolla", 20000.0m);
+            Console.WriteLine("Welcome to the car store!");
 
-            Console.WriteLine("Car c is : " + c.Brand + " " + c.Model + " " + c.Price );
-            Console.WriteLine("Car d is : " + d.Brand + " " + d.Model + " " + d.Price );
+            int action = chooseAction();
 
-            Store s = new Store();
+            while(action != 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("You picked: " + action);
+                Console.WriteLine();
 
-            s.ShoppingList.Add(c);
-            s.ShoppingList.Add(d);
-            decimal total = s.Checkout();
+                switch(action)
+                {
+                    case 1:
+                        Console.WriteLine("Adding a car to the store");
+                        break;
+                    case 2:
+                        Console.WriteLine("Adding a car to the shopping list");
+                        break;
+                    case 3:
+                        Console.WriteLine("Checking out");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
 
-            Console.WriteLine("Total price is : " + total);
+                action = chooseAction();
+            }
 
-            Console.ReadLine();
+            
         }
+
+        static public int chooseAction()
+        {
+            Console.WriteLine("Choose an action:");
+            Console.WriteLine("1. Add a car to the store");
+            Console.WriteLine("2. Add a car to the shopping list");
+            Console.WriteLine("3. Checkout");
+            Console.WriteLine("0. Exit");
+
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int choice))
+            {
+                Console.WriteLine("---------------------------");
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number");
+            }
+            return choice;
+        }
+
+        //Car c = new Car("Volvo", "V70", 35000.0m);
+        //Car d = new Car("Toyota", "Corolla", 20000.0m);
+
+        //Console.WriteLine("Car c is : " + c.Brand + " " + c.Model + " " + c.Price );
+        //Console.WriteLine("Car d is : " + d.Brand + " " + d.Model + " " + d.Price );
+
+        //    Store s = new Store();
+
+        //s.ShoppingList.Add(c);
+        //    s.ShoppingList.Add(d);
+        //    decimal total = s.Checkout();
+
+        //Console.WriteLine("Total price is : " + total);
+
+        //Console.ReadLine();
     }
 }
