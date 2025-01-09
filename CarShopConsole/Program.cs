e23bd918ba00f4a122faf8f1cfd681f117a86c37
Amaliebra <amaliebratland@gmail.com>
2025-01-09 10:42:30 +0100

    added new class

--- CarShopConsole/Program.cs
+++ CarShopConsole/Program.cs
@@ -1,7 +1,18 @@
-﻿namespace CarShopConsole
+﻿using CarClassLibrary;
+
+namespace CarShopConsole
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            Car c = new Car();
+            Car d = new Car("Toyota", "Corolla", 20000.0m);
 
+            Console.WriteLine("Car c is : " + c.Brand + " " + c.Model + " " + c.Price );
+            Console.WriteLine("Car d is : " + d.Brand + " " + d.Model + " " + d.Price );
+
+            Console.ReadLine();
+        }
     }
 }
