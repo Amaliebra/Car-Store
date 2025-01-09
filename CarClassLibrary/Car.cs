a28aac44a46fae0b9ba9eb535b10720ddc0a753b
Amaliebra <amaliebratland@gmail.com>
2025-01-09 10:12:24 +0100

    added 2 contructors

--- CarClassLibrary/Car.cs
+++ CarClassLibrary/Car.cs
@@ -11,5 +11,19 @@ namespace CarClassLibrary
         public string Brand { get; set; }
         public string Model { get; set; }
         public decimal Price { get; set; }
+        
+        public Car ()
+        {
+            Brand = "Unknown";
+            Model = "Unknown";
+            Price = 0.0m;
+        }
+
+        public Car (string a, string b, decimal c)
+        {
+            Brand = a;
+            Model = b;
+            Price = c;
+        }
     }
 }
